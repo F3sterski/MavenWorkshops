5cc94ee6839113aa69b8e8a7a52ef8acc418761f
F3sterski <szzkola121@wp.pl>
2022-05-12 20:31:33 +0200

    add plugin

--- src/test/java/example2Test.java
+++
@@ -1,8 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class example2Test {
-    @Test
-    public void test2(){
-        System.out.println("hehesszki");
-    }
-}
--- src/test/java/exampleTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-
-public class exampleTest {
-    @Test
-    public void test() {
-        System.out.println("Tescik\n\n\n\n\n\n\n\n\n++++\n\n\n\n\n");
-        System.out.println(1/0);
-    }
-}
