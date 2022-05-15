b76301ef21db48c5e3f752a01ecad68238550402
F3sterski <szzkola121@wp.pl>
2022-05-15 19:46:03 +0200

    refactor next part

--- CookieFinder/src/main/java/Main.java
+++ CookieFinder/src/main/java/Main.java
@@ -5,18 +5,23 @@
 
 //import com.google.common.math.IntMath;
 import com.sun.jersey.json.impl.writer.JsonEncoder;
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.hadoop.conf.StorageUnit;
+import org.tritonus.share.midi.MidiUtils;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 
 public class Main {
     public static void main(String[] args)  {
 
-        System.out.println(StorageUnit.GB);  //compile Hadoop
+//        System.out.println(StorageUnit.GB);  //compile Hadoop
 
 //        System.out.println(IntMath.binomial(6, 3));  //test Guava
 
-//        System.out.println(DateUtils.MILLIS_PER_DAY); //compile lang3
+//        MidiUtils.get14bitLSB(23);//provided lang3
 
 //        System.out.println(KrbUtil.ANONYMOUS_PRINCIPAL); // runtime hadoop
 
